9ae2a06815e11656bf1e4907b09645d75c024d9f
unknown <iLalLoTiX>
2020-03-31 09:37:14 -0600

    typescript

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
-    apiKey: "AIzaSyBB8LbjhrExxKHlyec4SuDswdGdPiTisTs",
-    authDomain: "yayo-6cbdd.firebaseapp.com",
-    databaseURL: "https://yayo-6cbdd.firebaseio.com",
-    projectId: "yayo-6cbdd",
-    storageBucket: "yayo-6cbdd.appspot.com",
-    messagingSenderId: "148746714612",
-    appId: "1:148746714612:web:138c3553ce2248099b8da1"
+    apiKey: 'AIzaSyBB8LbjhrExxKHlyec4SuDswdGdPiTisTs',
+    authDomain: 'yayo-6cbdd.firebaseapp.com',
+    databaseURL: 'https://yayo-6cbdd.firebaseio.com',
+    projectId: 'yayo-6cbdd',
+    storageBucket: 'yayo-6cbdd.appspot.com',
+    messagingSenderId: '148746714612',
+    appId: '1:148746714612:web:138c3553ce2248099b8da1'
   }
 };
 
